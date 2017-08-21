5c53e929a32dfa9990dcbcc01871565c10cbd6f8
Berto Yáñez <berto@ber.to>
2017-08-21 12:35:06 +0200

    #2 Frontpage

--- front/app/js/components/Home/Home.jsx
+++ front/app/js/components/Home/Home.jsx
@@ -4,26 +4,38 @@ import { Link } from 'react-router-dom';
 import styles from './Home.css';
 
 const Home = () =>
-  <header className={styles.home}>
-    <h1 className={styles.title}>Master of Okolicne</h1>
-    <p className={styles.highlight}>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-      repudiandae, distinctio doloremque repellat quo blanditiis, sequi
-      laudantium quis repellendus eveniet magnam quasi tempora in exercitationem
-      itaque alias minima quibusdam dolore.
-    </p>
-    <nav>
-      <ul className={styles.navigation}>
-        <li>
-          <Link className={styles.button} to="/create">
-            Create your own
-          </Link>
-          <Link className={styles.button} to="/gallery">
-            Gallery
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>;
+  <div>
+    <video
+      className={styles.videoBG}
+      src="/media/videos/home.mp4"
+      autoPlay
+      loop
+    />
+    <header className={styles.home}>
+      <h1 className={styles.title}>
+        Učeň z <br />Okoličného
+      </h1>
+      <h2 className={styles.subtitle}>
+        Majster hľadá nového učňa, navštív jeho dielňu
+      </h2>
+      <nav>
+        <ul className={styles.buttonList}>
+          <li>
+            <Link className={styles.mainButton} to="/create">
+              <span>
+                Vstúpiť <br /> do dielne<br />
+                <img className={styles.iconImg} src="/media/elements/B-enterF.svg"/>
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.smallButton} to="/gallery">
+              Galéria
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </div>;
 
 export default Home;
