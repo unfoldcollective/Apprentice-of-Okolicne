ec0e793a50f83c4c2e980b755790e0a5319be0cb
Berto Yáñez <berto@ber.to>
2017-08-26 21:58:54 +0200

    Fixes #29

--- front/app/js/components/Create/SaveDialog/Keyboard/Keyboard.jsx
+++ front/app/js/components/Create/SaveDialog/Keyboard/Keyboard.jsx
@@ -1,39 +1,83 @@
 import React from 'react';
 import Button from '../../Button/Button.jsx';
 import styles from './Keyboard.css';
+import cn from 'classnames';
 
-const Keyboard = ({ pushAction, popAction }) => {
-  const chars = [
-    '1234567890'.split(''),
-    'QWERTZUIOP'.split(''),
-    'ASDFGHJKL'.split(''),
-    'YXCVBNM'.split('')
-  ];
+export default class Keyboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      caps: true,
+      chars: [
+        '1234567890'.split(''),
+        'QWERTZUIOP'.split(''),
+        'ASDFGHJKL'.split(''),
+        'YXCVBNM'.split('')
+      ]
+    };
+  }
 
-  const rows = chars.map((row, i) =>
-    <div key={`row_${i}`}>
-      {row.map((c, i) =>
-        <Button className={styles.key} key={`char_${i}`} char={c} action={pushAction.bind(null, c)}>{c}</Button>
-      )}
-    </div>
-  );
-
-  return (
-    <div>
-      <div>
-        {rows}
+  render() {
+    const rows = this.state.chars.map((row, i) =>
+      <div key={`row_${i}`}>
+        {row.map((c, i) => {
+          c = this.state.caps ? c : c.toLowerCase();
+          return (
+            <Button
+              className={styles.key}
+              key={`char_${i}`}
+              char={c}
+              action={this.props.pushAction.bind(null, c)}
+            >
+              {c}
+            </Button>
+          );
+        })}
       </div>
+    );
+
+    return (
       <div>
-        <Button className={styles.key} action={pushAction.bind(null, '.')}>.</Button>
-        <Button className={styles.key} action={pushAction.bind(null, '@')}>@</Button>
-        <Button className={styles.space} action={pushAction.bind(null, ' ')}>SPACE</Button>
+        <div>
+          {rows}
+        </div>
+        <div>
+          <Button
+            className={cn(styles.widekey, {
+              [styles.negative]: this.state.caps
+            })}
+            action={e => this.setState({ caps: !this.state.caps })}
+          >
+            CAPS
+          </Button>
+          <Button
+            className={styles.key}
+            action={this.props.pushAction.bind(null, '.')}
+          >
+            .
+          </Button>
+          <Button
+            className={styles.space}
+            action={this.props.pushAction.bind(null, ' ')}
+          >
+            SPACE
+          </Button>
 
-        <Button className={styles.widekey} char="Delete" action={popAction}>
-          DEL
-        </Button>
+          <Button
+            className={styles.key}
+            action={this.props.pushAction.bind(null, '@')}
+          >
+            @
+          </Button>
+          <Button
+            className={styles.widekey}
+            char="Delete"
+            action={this.props.popAction}
+          >
+            DEL
+          </Button>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Keyboard;
+    );
+  }
+}
