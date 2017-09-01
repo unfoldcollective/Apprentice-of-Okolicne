0a4f0a82acc4a4f21740e99bd2c810efd8b5324d
Berto Yáñez <berto@ber.to>
2017-09-01 14:07:02 +0200

    Fixes #68 @phillchill the regular shrinking is because rendering performance

--- front/app/js/components/Gallery/Gallery.jsx
+++ front/app/js/components/Gallery/Gallery.jsx
@@ -49,7 +49,7 @@ class Gallery extends React.Component {
       percentTimeout: 1
     };
 
-    this.maxSeconds = 60;
+    this.maxSeconds = 20;
     this.inactivity = 0;
 
     superagent.get('/api').then(d => {
