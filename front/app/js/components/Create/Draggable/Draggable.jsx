4129e6f4b5542e69a03bf836315c00b2e5fc4f45
Berto Yáñez <berto@ber.to>
2017-09-05 18:40:41 +0200

    Better touch events management

--- front/app/js/components/Create/Draggable/Draggable.jsx
+++ front/app/js/components/Create/Draggable/Draggable.jsx
@@ -63,6 +63,7 @@ export default class Draggable extends React.Component {
   render() {
     return (
       <div
+        className={styles.dragTarget}
         ref={el => {
           this.draggable = el;
         }}
