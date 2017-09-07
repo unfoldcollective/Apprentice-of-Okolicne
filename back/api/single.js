0587c6db4b50d236997760046c8652e5d5ba2dc0
Berto Yáñez <berto@ber.to>
2017-09-07 16:42:51 +0200

    Fix deletion

--- back/api/single.js
+++ back/api/single.js
@@ -23,4 +23,24 @@ module.exports = (app, db) => {
       });
     })
   );
+
+  app.get(
+    '/api/id/:id/nuke',
+    wrap(function*(req, res, next) {
+      const id = req.params.id;
+
+      const data = yield storage.remove({ _id: new ObjectID(id) });
+
+      if (!data) {
+        const err = new Error('Element not found in the database');
+        err.status = 404;
+        return next(err);
+      }
+
+      return res.send({
+        error: false,
+        data
+      });
+    })
+  );
 };
