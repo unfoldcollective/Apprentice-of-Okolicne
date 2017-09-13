8435e2ba0d8626178c5ab1bd57f263e20f7a50c6
Berto Yáñez <berto@ber.to>
2017-09-13 11:36:04 +0200

    Fixes #101 again

--- front/app/js/components/Create/SaveDialog/SideInfo.jsx
+++ front/app/js/components/Create/SaveDialog/SideInfo.jsx
@@ -28,21 +28,29 @@ class SideInfo extends React.Component {
         <p className={styles.text}>{T.translate('save.help')}</p>
         <footer className={styles.footer}>
           <div className={styles.mainButtons}>
-          {this.state.saving ?
-            <img className={styles.saving} src="/media/elements/I-LoadingSpinner.svg" />
-          :
-          <Button
-            disabled={this.props.processing}
-            className={styles.button}
-            action={this.props.nextStep}
-          >
-            <img
-              className={styles.buttonImage}
-              src="/media/elements/B-completeF.svg"
-            />
-          </Button>
-
-          }
+            {this.state.saving ? (
+              <img
+                className={styles.saving}
+                src="/media/elements/I-LoadingSpinner.svg"
+              />
+            ) : (
+              <Button
+                disabled={this.props.processing}
+                className={styles.button}
+                action={this.props.nextStep}
+              >
+                <img
+                  className={styles.buttonImage}
+                  src={
+                    this.props.saveStep ? (
+                      '/media/elements/B-completeF.svg'
+                    ) : (
+                      '/media/elements/B-nextF.svg'
+                    )
+                  }
+                />
+              </Button>
+            )}
           </div>
 
           <div className={styles.back}>
